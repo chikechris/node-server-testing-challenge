8cb900fe4622f5f218214ef993a7e86e4b736ebc
chikechris <cchike2017@gmail.com>
2019-09-19 15:56:43 -0400

    basic test passed

---
+++ api/server.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+const server = require('./server.js')
+
+describe('server.js root route', () => {
+  it('should return an ok status code from the index route', async () => {
+    const response = await request(server).get('/')
+    console.log(response.text)
+    expect(response.status).toEqual(201)
+    expect(response.text).toHaveLength(43)
+  })
+})
