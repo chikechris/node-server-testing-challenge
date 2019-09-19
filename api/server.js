c954f5f374703c7d9854d57cd7b34d22b390374c
chikechris <cchike2017@gmail.com>
2019-09-19 16:49:34 -0400

    tested register endpoint

--- api/server.js
+++ api/server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 
+const authRoute = require('../routes/authRoute/authRoute.js')
+
 const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.use('/auth', authRoute)
 
 server.use('/', (req, res) => {
   res.status(201).json({ message: 'server is up' })
 })
 
 module.exports = server
+
--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const server = require('./server.js')
+const db = require('../data/dbConfig.js')
 
 describe('server.js root route', () => {
   it('should return an ok status code from the index route', async () => {
@@ -11,6 +12,20 @@ describe('server.js root route', () => {
   it('should return a response object of {message: "server is up"}', async () => {
     const response = await request(server).get('/')
     expect(response.body).toEqual({ message: 'server is up' })
+  })
+})
+
+describe('authRoute.js', () => {
+  afterAll(async () => {
+    await db('users').truncate()
+  })
+  describe('POST /auth/register', () => {
+    it('should return the id number of the newly created user', async () => {
+      const response = await request(server)
+        .post('/auth/register')
+        .send({ username: 'chris', password: 'chris' })
+      expect(response.body).toEqual(1)
+    })
   })
 })
 
